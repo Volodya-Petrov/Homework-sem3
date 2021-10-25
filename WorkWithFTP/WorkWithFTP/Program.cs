f57c407e2b7cc51fb326aa12c51be923a780e79e
Volodya Petrov <71704795+Volodya-Petrov@users.noreply.github.com>
2021-10-26 01:15:22 +0300

    -added server class, but it doesn't work

---
+++ WorkWithFTP/WorkWithFTP/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WorkWithFTP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var server = new MyFTP(8888);
+            server.Run();
+            for (int i = 0; i < 3; i++)
+            {
+                var index = i;
+                Task.Run(async () =>
+                {
+                    var client = new TcpClient("localhost", 8888);
+                    var stream = client.GetStream();
+                    var streamWriter = new StreamWriter(stream);
+                    var streamReader = new StreamReader(stream);
+                    await streamWriter.WriteAsync("1 .");
+                    var text = await streamReader.ReadLineAsync();
+                    Console.WriteLine($"это {index} крутой чувак\n{text}");
+                });
+            }
+            Thread.Sleep(100000);
+        }
+    }
+}
