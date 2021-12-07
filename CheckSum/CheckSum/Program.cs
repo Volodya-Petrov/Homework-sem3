547813f4d3d184cfc2933e5fba3037f25adf42e5
Volodya Petrov <89377379140@mail.ru>
2021-12-07 16:51:46 +0300

    added test

---
+++ CheckSum/CheckSum/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+namespace CheckSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var checkSum = new CheckSum();
+            var result = checkSum.Calculate(args[0]);
+            Console.WriteLine(BitConverter.ToString(result));
+        }
+    }
+}
---
+++ CheckSum/Test/Kek/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace CheckSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = Console.ReadLine();
+            var checkSum = new CheckSum();
+            var result = checkSum.Calculate(path);
+            Console.WriteLine(BitConverter.ToString(result));
+        }
+    }
+}
